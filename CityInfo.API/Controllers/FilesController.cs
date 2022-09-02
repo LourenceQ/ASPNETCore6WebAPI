6b98a6c7600295e69e97723b98370b1b13084d07
LourenceQ <lqfmecatronica@gmail.com>
2022-09-01 23:37:08 -0300

    Implementing fileController and adding capability to output response as sml

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            var pathToFile = "getting-started-with-rest-slides.pdf";
+
+            if (!System.IO.File.Exists(pathToFile))
+                return NotFound();
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+
+            return File(bytes, "text/plain", Path.GetFileName(pathToFile));
+                
+        }
+    }
+}
